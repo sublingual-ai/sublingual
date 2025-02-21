e456b588369fbcbe6492783e37cb8cd765ab2c7d
bowman <dylanbowman314@gmail.com>
2025-02-21 14:45:51 -0800

    gets it working

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="subl",
     version="0.1.0",
+    author="Matthew Tang and Dylan Bowman",
+    author_email="founders@sublingual.ai",
+    description="No-code LLM production app evals.",
+    url="https://github.com/sublingual-ai/sublingual-eval",
     packages=['sublingual_eval', 'dashboard'],  # Explicitly list the packages
     entry_points={
         'console_scripts': [
@@ -10,4 +14,5 @@ setup(
             'subl-server=dashboard.run_servers:main',
         ],
     },
+    license="MIT",
 )

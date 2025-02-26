30463635378c07edd88fd7c4fd9c7eb39152099e
matt <mtang317@gmail.com>
2025-02-26 02:28:14 +0000

    0.1.8 + package frontend changes

--- package_src/setup.py
+++ package_src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="subl",
-    version="0.1.7",
+    version="0.1.8",
     author="Matthew Tang and Dylan Bowman",
     author_email="founders@sublingual.ai",
     description="No-code LLM production app evals.",
